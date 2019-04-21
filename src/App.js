924b5275e3187b72bea8e7892251dd76d048d396
Shug0 <thomas@needacoffee.fr>
2019-04-21 21:02:51 +0200

    Moving client into proper repository

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import StartPage from './pages/StartPage/StartPage'
+import HomePage from './pages/HomePage/HomePage'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { userFormValidationSchema } from './forms/schemas'
+import { getCookieUser } from './helpers/user'
+
+// import io from 'socket.io-client'
+// const socket = io.connect('http://localhost:8080')
+
+class App extends Component {
+  checkUserCookies = () => userFormValidationSchema.isValidSync(getCookieUser())
+
+  render () {
+    return (
+      <Router>
+        <main className='Wrapper'>
+          <Route
+            exact
+            path='/'
+            render={() =>
+              !this.checkUserCookies() ? <Redirect to='/setup' /> : <HomePage user={getCookieUser()} />
+            }
+          />
+          <Route path='/setup' component={StartPage} />
+        </main>
+      </Router>
+    )
+  }
+}
+
+export default App
