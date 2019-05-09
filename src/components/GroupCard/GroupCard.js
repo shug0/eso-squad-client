a3c521dd1353e65cd46f3bd5f51a13b6e70d78d4
Shug0 <thomas@needacoffee.fr>
2019-05-09 20:06:52 +0200

    feat(GroupCard): handle veteran case, fix styles, prepare player count

--- src/components/GroupCard/GroupCard.js
+++ src/components/GroupCard/GroupCard.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import sum from 'lodash/sum'
 import './GroupCard.scss'
+import { string, object } from 'prop-types'
 import events from '../../constants/data/eventsMap'
 import { PlayersIcons } from '../Icons'
 import { COLORS } from '../../constants/theme'
 
 class GroupCard extends Component {
   getImgById = (id) => {
-    const path = `${process.env.PUBLIC_URL}/assets/illustrations/low/${id}.jpg`
-    return path.replace(/(-i.jpg)|(-ii.jpg)/, '.jpg')
+    const path =
+      `${process.env.PUBLIC_URL}/assets/illustrations/low/${id}.jpg`
+        .replace('-veteran', '')
+        .replace(/(-i.jpg)|(-ii.jpg)/, '.jpg')
+
+    return path
   }
 
   getHeaderStyles = (url) => ({
@@ -16,26 +23,37 @@ class GroupCard extends Component {
     backgroundColor: COLORS.backgroundDark
   })
 
+  static propTypes = {
+    id: string.isRequired,
+    eventId: string.isRequired,
+    playersTemplate: object.isRequired
+  }
+
   render () {
-    const { eventId } = this.props
+    const { eventId, playersTemplate, id } = this.props
     const bgPath = this.getImgById(eventId)
     const bgStyles = this.getHeaderStyles(bgPath)
 
+    const playersInGroup = 0
+    const playerLimit = sum(Object.values(playersTemplate))
+
     return (
-      <article className='Card GroupCard Appear'>
-        <header className='GroupCard__header' style={bgStyles} >
-          <div className='GroupCard__header__filter'>
-            <h3 className='GroupCard__header__players'>
-              <PlayersIcons />
-              2/4
-            </h3>
-            <h3 className='GroupCard__header__title'>{events[eventId].name}</h3>
+      <Link to={`/group/${id}`} className='GroupCard__link'>
+        <article className='Card GroupCard Appear'>
+          <header className='GroupCard__header' style={bgStyles} >
+            <div className='GroupCard__header__gradiant'>
+              <div className='GroupCard__header__players'>
+                <PlayersIcons />
+                {playersInGroup}/{playerLimit}
+              </div>
+              <h3 className='GroupCard__header__title'>{events[eventId].name}</h3>
+            </div>
+          </header>
+          <div className='GroupCard__content'>
+            Veteran
           </div>
-        </header>
-        <div className='GroupCard__content'>
-          Veteran
-        </div>
-      </article>
+        </article>
+      </Link>
     )
   }
 }
